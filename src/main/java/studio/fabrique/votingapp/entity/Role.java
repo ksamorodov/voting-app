5df38fdb14d76902046a2ce5810df6bb80da3173
ksamorodov <ksamorodov@yandex.ru>
2021-07-31 08:52:37 +0300

    Add user entity and role entity

---
+++ src/main/java/studio/fabrique/votingapp/entity/Role.java
@@ -0,0 +1,42 @@
+package studio.fabrique.votingapp.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "t_role")
+public class Role implements GrantedAuthority {
+    @Id
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Transient
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+
+    public Role() {
+    }
+
+    public Role(Long id) {
+        this.id = id;
+    }
+
+    public Role(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getName();
+    }
+}
